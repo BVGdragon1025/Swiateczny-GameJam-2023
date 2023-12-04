d5e76bb095ab051d6890db0fa1d89831fac1f6b1
Dominik Nawrot <dnawrot1025@gmail.com>
2023-12-04 19:25:10 +0100

    Start

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Private variables
+    private float _defaultSpeed;
+    private float _defaultAccSpeed;
+    private float _accelerationRate;
+    [SerializeField] private float _currentSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveCharacter()
+    {
+        
+    }
+}
