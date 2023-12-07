85251987d8174032b9195166201eaf459b69d4da
Dominik Nawrot <dnawrot1025@gmail.com>
2023-12-07 13:38:24 +0100

    Some changes, lol

--- Assets/Scripts/Player/PlayerCarController.cs
+++ Assets/Scripts/Player/PlayerCarController.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerCarController : MonoBehaviour
 {
-    public List<AxleInfo> axleInfos; // the information about each individual axle
-    public float maxMotorTorque; // maximum torque the motor can apply to wheel
-    public float maxSteeringAngle; // maximum steer angle the wheel can have
-
+    [SerializeField] private List<AxleInfo> _axleInfos; // the information about each individual axle
+    [SerializeField] private float _maxMotorTorque; // maximum torque the motor can apply to wheel
+    [SerializeField] private float _maxSteeringAngle; // maximum steer angle the wheel can have
+    [SerializeField] private float _maxSpeed; //maximum value of speed, when player is not giving any input. In km/h.
+    [SerializeField] private float _maxSpeedAccelerated; //maximum value of speed, when player is giving an input. In km/h.
+    [SerializeField] private float _currentSteeringAngle;
+    [SerializeField] private float _currentSpeed; //in km/h
+ 
     private Rigidbody _rb;
 
     private void Start()
@@ -15,15 +20,20 @@ public class PlayerCarController : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        _currentSpeed = _rb.velocity.magnitude * 3.6f;
+    }
+
     public void FixedUpdate()
     {
         float forwardInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        float motor = maxMotorTorque * forwardInput;
-        float steering = maxSteeringAngle * horizontalInput;
+        //float motor = _maxMotorTorque * forwardInput * Time.deltaTime;
+        float steering = _maxSteeringAngle * horizontalInput;
 
-        foreach (AxleInfo axleInfo in axleInfos)
+        foreach (AxleInfo axleInfo in _axleInfos)
         {
             if (axleInfo.steering)
             {
@@ -32,15 +42,76 @@ public class PlayerCarController : MonoBehaviour
             }
             if (axleInfo.motor)
             {
-                axleInfo.leftWheel.motorTorque = motor;
-                axleInfo.rightWheel.motorTorque = motor;
+                axleInfo.leftWheel.motorTorque = MoveCharacter(forwardInput);
+                axleInfo.rightWheel.motorTorque = MoveCharacter(forwardInput);
+                axleInfo.leftWheel.brakeTorque = ReduceSpeed(forwardInput);
+                axleInfo.rightWheel.brakeTorque = ReduceSpeed(forwardInput);
             }
         }
     }
 
     float MoveCharacter(float vInput)
     {
+        float motorTorque;
+
+        if(vInput == 0)
+        {
+            if(_currentSpeed < _maxSpeed)
+            {
+                //var multiplier = Mathf.Lerp()
+                motorTorque = _maxMotorTorque;
+                return motorTorque;
+            }
+            else
+            {
+                _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _maxSpeed);
+            }
+        }
+
+        if(vInput != 0)
+        {
+            if(_currentSpeed < _maxSpeedAccelerated)
+            {
+                motorTorque = (_maxMotorTorque * 2) * vInput;
+                return motorTorque;
+            }
+            else
+            {
+                _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _maxSpeedAccelerated);
+            }
+            
+        }
+
         return 0;
+
+    }
+
+    void MoveCharacterRB(float vInput)
+    {
+
+    }
+
+    float ReduceSpeed(float vInput)
+    {
+        float brakeTorque;
+
+        if(vInput == 0 && _currentSpeed > _maxSpeed)
+        {
+            brakeTorque = _maxMotorTorque;
+            return brakeTorque;
+        }
+        else if(vInput != 0 && _currentSpeed > _maxSpeedAccelerated)
+        {
+            brakeTorque = _maxMotorTorque * 2;
+            return brakeTorque;
+        }
+        else
+        {
+            brakeTorque = 0;
+            return brakeTorque;
+        }
+
+
     }
 
 }
